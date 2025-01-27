be0cc7c9d68a09dc2dbcc248026bed3e35e2174c
Elisa <elisa.salvatore110@gmail.com>
2025-01-27 17:33:38 +0100

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header"></header>
-		</div>
-	);
+	//API URL
+	const apiUrl = "https://random-data-api.com/api/users/random_user?size=10";
+	// State to store the fetched data
+	const [data, setData] = useState([]);
+
+	// Fetch data from the API when the component mounts
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(apiUrl);
+				const result = await response.json();
+				setData(result);
+				console.log(result);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return <div className="App"></div>;
 }
 
 export default App;
